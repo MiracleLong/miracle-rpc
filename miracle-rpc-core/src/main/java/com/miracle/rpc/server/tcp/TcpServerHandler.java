96ea8f120dc7ccf1a16ca66c46bf2f6acc91a061
Miracle <2893496202@qq.com>
2024-03-14 23:56:28 +0800

    fix: 封装半包粘包处理器

--- miracle-rpc-core/src/main/java/com/miracle/rpc/server/tcp/TcpServerHandler.java
+++ miracle-rpc-core/src/main/java/com/miracle/rpc/server/tcp/TcpServerHandler.java
@@ -7,6 +7,7 @@ import com.miracle.rpc.protocol.ProtocolMessageDecoder;
 import com.miracle.rpc.protocol.ProtocolMessageEncoder;
 import com.miracle.rpc.protocol.ProtocolMessageTypeEnum;
 import com.miracle.rpc.registry.LocalRegistry;
+import io.netty.channel.ChannelHandlerContext;
 import io.vertx.core.Handler;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.net.NetSocket;
@@ -23,8 +24,7 @@ public class TcpServerHandler implements Handler<NetSocket> {
 
     @Override
     public void handle(NetSocket netSocket) {
-        // 处理连接
-        netSocket.handler(buffer -> {
+        TcpBufferHandlerWrapper bufferHandlerWrapper = new TcpBufferHandlerWrapper(buffer -> {
             // 接受请求，解码
             ProtocolMessage<RpcRequest> protocolMessage;
             try {
@@ -63,5 +63,6 @@ public class TcpServerHandler implements Handler<NetSocket> {
                 throw new RuntimeException("协议消息编码错误");
             }
         });
+        netSocket.handler(bufferHandlerWrapper);
     }
 }
