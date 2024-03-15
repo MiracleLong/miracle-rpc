2672871df06ef56dff69987834377b72bf176ce4
Miracle <2893496202@qq.com>
2024-03-15 23:43:33 +0800

    perf: 轮询负载均衡器

---
+++ miracle-rpc-core/src/main/java/com/miracle/rpc/loadbalancer/RoundRobinLoadBalancer.java
@@ -0,0 +1,35 @@
+package com.miracle.rpc.loadbalancer;
+
+import com.miracle.rpc.model.ServiceMetaInfo;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author dargon
+ * @create 2024-03-14
+ * @description 轮询负载均衡
+ */
+public class RoundRobinLoadBalancer implements LoadBalancer{
+
+    /**
+     * 当前轮询的下标
+     */
+    private final AtomicInteger currentIndex = new AtomicInteger(0);
+
+    @Override
+    public ServiceMetaInfo select(Map<String, Object> requestParams, List<ServiceMetaInfo> serviceMetaInfoList) {
+        if (serviceMetaInfoList.isEmpty()) {
+            return null;
+        }
+        // 只有一个服务，无需轮询
+        int size = serviceMetaInfoList.size();
+        if (size == 1) {
+            return serviceMetaInfoList.get(0);
+        }
+
+        int index = currentIndex.getAndIncrement() % size;
+        return serviceMetaInfoList.get(index);
+    }
+}
